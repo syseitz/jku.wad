d7bda2176a9da10817a60ffb7d975ad29f21245a
Yannick Seitz <a01508252@unet.univie.ac.at>
2025-06-16 13:51:14 +0200

    modified:   doom_arena/reward.py

--- doom_arena/reward.py
+++ doom_arena/reward.py
@@ -22,16 +22,10 @@ class VizDoomReward:
         self._step += 1
         _ = player_id
         rwd_hit = 2.0 * (game_var["HITCOUNT"] - game_var_old["HITCOUNT"])
-        rwd_hit_taken = -0.1 * (game_var["HITS_TAKEN"] - game_var_old["HITS_TAKEN"])
-        rwd_frag = 100.0 * (game_var["FRAGCOUNT"] - game_var_old["FRAGCOUNT"])
-        # rwd_health = (game_var["HEALTH"] - game_var_old["HEALTH"])
-        # rwd_dead = -10.0 if (game_var["DEAD"] - game_var_old["DEAD"]) > 0 else 0.0
+        rwd_hit_taken = -1.0 * (game_var["HITS_TAKEN"] - game_var_old["HITS_TAKEN"])
+        rwd_frag = 10.0 * (game_var["FRAGCOUNT"] - game_var_old["FRAGCOUNT"])
+        rwd_health = 0.1 * (game_var["HEALTH"] - game_var_old["HEALTH"])
+        rwd_dead = -10.0 if (game_var["DEAD"] - game_var_old["DEAD"]) > 0 else 0.0
+        rwd_ammo = -0.5 * (game_var_old["AMMO"] - game_var["AMMO"]) if "AMMO" in game_var else 0.0
 
-        return (
-            # vizdoom_reward,
-            rwd_hit,
-            rwd_hit_taken,
-            rwd_frag,
-            # rwd_health,
-            # rwd_dead,
-        )
+        return (rwd_hit, rwd_hit_taken, rwd_frag, rwd_health, rwd_dead, rwd_ammo)
